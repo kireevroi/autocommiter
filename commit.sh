2b1ce33871a2755083b35047ad1f9a1a61d89f44
kireevroi <kireevroi@gmail.com>
2023-03-29 22:56:18 +0400

    auto

---
+++ commit.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+git config user.name "kireevroi"
+git config user.email "kireevroi@gmail.com"
+# loop randomly from 0 to 4
+R=$(( $RANDOM % 5 ))
+for (( i=0; i<$R; i++ )); do
+		# get a random number between 0 and 4
+		datetime=$(date +%s)
+		echo $datetime >> commits.txt
+		# add the file to the index
+		git add commits.txt
+		# commit the file
+		git commit -m "commit $datetime"
+		# push the file to the remote repository
+		git push origin master
+done
